45be4c5b9dfc0292cccae24763717e708e55a0b4
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-07 17:37:41 +0900

    7/07 연습문제 5번 풀이완료

--- src/com/javaex/problem05/StringUtil.java
+++ src/com/javaex/problem05/StringUtil.java
@@ -2,8 +2,17 @@ package com.javaex.problem05;
 
 public class StringUtil {
     
-    public static String concatString(){
-       
+    public static String concatString(String[] strArray){
+    	
+    	String resultStr="";
+    	
+    	for(int i=0;i<strArray.length;i++) {
+    		resultStr+=strArray[i];
+    	}
+		
+		return resultStr;
+		
+    			
         //메소드 내용작성
         
     }
