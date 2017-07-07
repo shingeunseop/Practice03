c9502d36a1ad4f0976bca8239b24a0cbe9cd0c24
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-07 18:43:38 +0900

    7/07 연습문제 07 문제풀이완료

--- src/com/javaex/problem07/Account.java
+++ src/com/javaex/problem07/Account.java
@@ -5,7 +5,31 @@ public class Account {
     private String accountNo;
     private int balance;
     
+    public Account(String r) {
+    	
+    }
     //생성자 작성
+    public void deposit(int money) {
+    	balance+=money;
+    }
+    public void withdraw(int money) {
+    	balance=balance-money;
+    }
+    public void showBalance() {
+    	System.out.println(balance);
+    }
+    public void setAccountNo(String accountNo) {
+    	this.accountNo=accountNo;
+    }
+    public void setBalance(int balance) {
+    	this.balance=balance;
+    }
+    public String accountNo() {
+    	return accountNo;
+    }
+    public int balance() {
+    	return balance;
+    }
     
     //필요한 메소드 작성
 
