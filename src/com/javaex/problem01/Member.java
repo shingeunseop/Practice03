53b01ca8daac2f55888990476fe83871ca5e79c1
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-07 16:22:58 +0900

    7/07 연습문제 1번

--- src/com/javaex/problem01/Member.java
+++ src/com/javaex/problem01/Member.java
@@ -1,5 +1,29 @@
 package com.javaex.problem01;
 
 public class Member {
+	private String id;
+	private String name;
+	private int point;
+	
+	
+	public void setId(String id) {
+		this.id=id;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+	public void setPoint(int point) {
+		this.point=point;
+	}
+	public String id() {
+		return id;
+	}
+	public String name() {
+		return name;
+	}
+	public int point() {
+		return point;
+	}
+	
 
 }
