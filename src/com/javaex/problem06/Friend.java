2dd73e24cdbc88cd3da34c6b7d2b258a1eacb304
myhome <myhome@175.195.155.96>
2017-07-12 23:58:02 +0900

    연습문제3 6번문제 finish

--- src/com/javaex/problem06/Friend.java
+++ src/com/javaex/problem06/Friend.java
@@ -2,17 +2,47 @@ package com.javaex.problem06;
 
 public class Friend {
 
-    private String name;
-    private String hp;
-    private String school;
+	private String name;
+	private String hp;
+	private String school;
+	
+	public Friend() {}
 
-    
-    //필요한 메소드 작성
-    
-    
-    
-    public void showInfo(){
-        System.out.println("이름:"+name+"  핸드폰:"+hp+"  학교:"+school);
-    }
+	public Friend(String name, String hp, String school) {
+	
+		this.name = name;
+		this.hp = hp;
+		this.school = school;
+	}
+
+	// 필요한 메소드 작성
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+	public void showInfo() {
+		System.out.println("이름:" + name + "  핸드폰:" + hp + "  학교:" + school);
+	}
 
 }
--- src/com/javaex/problem06/FriendApp.java
+++ src/com/javaex/problem06/FriendApp.java
@@ -1,30 +1,39 @@
 package com.javaex.problem06;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FriendApp {
 
     public static void main(String[] args) {
 
-        Friend[] friendArray = new Friend[3];
+        
+        
         Scanner sc = new Scanner(System.in);
+        ArrayList<Friend> flist=new ArrayList<Friend>();
 
         System.out.println("친구를 3명 등록해 주세요");
-
+        for(int i=0;i<3;i++) {
+        String str=sc.nextLine();
+        String[] div=str.split(" ");
         
-            // 친구정보 입력받기
-
+        Friend friendArray=new Friend(div[0],div[1],div[2]);
+        flist.add(friendArray);
+        }
+        for(int j=0;j<3;j++) {
+        System.out.println("이름:"+flist.get(j).getName()
+        		+" 핸드폰:"+flist.get(j).getHp()
+        		+" 학교:"+flist.get(j).getSchool());
+        }
+        	// 친구정보 입력받기
+        	
+       
             // 입력받은 친구정보를 공백으로 분리
-
             // Friend 객체 생성하여 데이터 넣기
-
-            // 배열에 추가하기
-
+           // 배열에 추가하기
         
         // 친구정보 출력
-        for (int i = 0; i < friendArray.length; i++) {
-            //친구정보 출력 메소드 호출
-        }
+       
 
         sc.close();
     }
